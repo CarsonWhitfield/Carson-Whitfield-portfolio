fa8fe7d85530d2db240f4ca1ebddab74a0d2aef6
CarsonWhitfield <cwhitfield123@hotmail.com>
2026-04-09 20:01:40 -0600

    Update page.tsx
    Signed-off-by: CarsonWhitfield <cwhitfield123@hotmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { ThemeToggle } from "./Theme";
 export default function Home() {
   return (
     <div
-      className="grid grid-rows-[40px_1fr_40px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-geist bg-cover bg-center bg-[url('/Background/Background_image.png')] dark:bg-[url('/Background/Background-light.png')]"
+      className="grid grid-rows-[40px_1fr_40px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-geist bg-cover bg-center bg-[url('./Background/Background_image.png')] dark:bg-[url('./Background/Background-light.png')]"
       
     >
       {/* Button for dark and light mode */}
