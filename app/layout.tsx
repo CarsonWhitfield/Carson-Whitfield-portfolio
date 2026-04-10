720b34a382f072718374ee90dcc4f41a3a96c590
CarsonWhitfield <cwhitfield123@hotmail.com>
2026-04-09 20:35:07 -0600

    Resolve merge conflict

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import type { Metadata } from "./next";
-import { Geist, Geist_Mono } from "./next/font/google";
-import { ThemeProvider } from "./next-themes";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
